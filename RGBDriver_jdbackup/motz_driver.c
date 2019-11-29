7964f8d9c2f5b6621c75bab7939accccecef8b2c
mmkser10 <mmkser10@gmail.com>
2019-11-29 01:48:03 -0800

    A

--- RGBDriver_jdbackup/motz_driver.c
+++ RGBDriver_jdbackup/motz_driver.c
@@ -35,26 +35,26 @@ static int motor180z_write(struct file*, const char*, size_t, loff_t*);
 
 
 static struct file_operations motor180z_fops=
-        {
-                .owner	=THIS_MODULE,
-                .open	=motor180z_open,
-                .release=motor180z_release,
-                .write	=motor180z_write,
-        };
+{
+	.owner	=THIS_MODULE,
+	.open	=motor180z_open,
+	.release=motor180z_release,
+	.write	=motor180z_write,
+};
 
 static int motor180z_init(void){
-    int result;
-    result=register_chrdev(motor180z_MAJOR, motor180z_NAME, &motor180z_fops);
-    if(result<0){
-        printk(KERN_WARNING"Can't get any major!\n");
-        return result;
-    }
+	int result;
+	result=register_chrdev(motor180z_MAJOR, motor180z_NAME, &motor180z_fops);
+	if(result<0){
+		printk(KERN_WARNING"Can't get any major!\n");
+		return result;
+	}
 }
 
 
 static void motor180z_exit(void){
-    unregister_chrdev(motor180z_MAJOR, motor180z_NAME);
-    printk("motor180z module removed.\n");
+	unregister_chrdev(motor180z_MAJOR, motor180z_NAME);
+	printk("motor180z module removed.\n");
 }
 
 
@@ -65,122 +65,122 @@ MODULE_LICENSE("GPL");
 
 
 static int motor180z_open(struct inode *inode, struct file *mfile){
-    if(motor180x_usage != 0)
-        return -EBUSY;
-    motor180z_usage=1;
+	if(motor180z_usage != 0)
+		return -EBUSY;
+	motor180z_usage=1;
 
-    motor180z_map=ioremap(GPIO_BASE, GPIO_SIZE);
+	motor180z_map=ioremap(GPIO_BASE, GPIO_SIZE);
 
-    if(!motor180z_map){
-        printk("error:mapping gpio memory");
-        iounmap(motor180z_map);
-        return -EBUSY;
-    }
-    motor180z=(volatile unsigned int*)motor180z_map;
+	if(!motor180z_map){
+		printk("error:mapping gpio memory");
+		iounmap(motor180z_map);
+		return -EBUSY;
+	}
+	motor180z=(volatile unsigned int*)motor180z_map;
 
-    *(motor180z+(GPIO/10))&=~(0x07<<(3*(GPIO%10)));
-    *(motor180z+(GPIO/10))|=(0x01<<(3*(GPIO%10)));
+	*(motor180z+(GPIO/10))&=~(0x07<<(3*(GPIO%10)));
+	*(motor180z+(GPIO/10))|=(0x01<<(3*(GPIO%10)));
 
-    return 0;
+	return 0;
 }
 
 static int motor180z_write(struct file *minode, const char *gdata, size_t length, loff_t *off_what){
-    char tmp_buf;
-    int result;
-    int frequency=20000;
-    int minAngle=544;
-    int maxAngle=2440;
-    int x;
-    unsigned int setClock;
-    unsigned int clrClock;
-    unsigned long delay = jiffies + 3*HZ;
+	char tmp_buf;
+	int result;
+	int frequency=20000;
+	int minAngle=544;
+	int maxAngle=2440;
+	int x;
+	unsigned int setClock;
+	unsigned int clrClock;
+	unsigned long delay = jiffies + 3*HZ;
 
 
-    result=copy_from_user(&tmp_buf, gdata, length);
-    if(result<0){
-        printk("Error:copy from user");
-        return result;
-    }
-    printk("data from user: %d\n", tmp_buf);
+	result=copy_from_user(&tmp_buf, gdata, length);
+	if(result<0){
+		printk("Error:copy from user");
+		return result;
+	}
+	printk("data from user: %d\n", tmp_buf);
 
 
-    if(tmp_buf==1){
-        for(x=0;x<=30;x++){
-            setClock=(minAngle+((maxAngle-minAngle)/90*x));
-            clrClock=frequency-setClock;
-            *(motor180z+7)=(0x01<<GPIO);
-            udelay(setClock);
-            *(motor180z+10)=(0x01<<GPIO);
-            udelay(clrClock);
-        }
-        while(time_before(jiffies, delay)){}
-        for(x=30;x>=0;x--){
-            setClock=(minAngle+((maxAngle-minAngle)/90*x));
-            clrClock=frequency-setClock;
-            *(motor180z+7)=(0x01<<GPIO);
-            udelay(setClock);
-            *(motor180z+10)=(0x01<<GPIO);
-            udelay(clrClock);
-        }
-    }
+	if(tmp_buf==1){
+		for(x=0;x<=30;x++){
+			setClock=(minAngle+((maxAngle-minAngle)/90*x));
+			clrClock=frequency-setClock;
+			*(motor180z+7)=(0x01<<GPIO);
+			udelay(setClock);
+			*(motor180z+10)=(0x01<<GPIO);
+			udelay(clrClock);
+		}
+		while(time_before(jiffies, delay)){}
+		for(x=30;x>=0;x--){
+			setClock=(minAngle+((maxAngle-minAngle)/90*x));
+			clrClock=frequency-setClock;
+			*(motor180z+7)=(0x01<<GPIO);
+			udelay(setClock);
+			*(motor180z+10)=(0x01<<GPIO);
+			udelay(clrClock);
+		}
+	}
 
-    if(tmp_buf==2){
-        for(x=90;x>=45;x--){
-            setClock=(minAngle+((maxAngle-minAngle)/90*x));
-            clrClock=frequency-setClock;
-            *(motor180z+7)=(0x01<<GPIO);
-            udelay(setClock);
-            *(motor180z+10)=(0x01<<GPIO);
-            udelay(clrClock);
-        }
-        while(time_before(jiffies, delay)){}
-        for(x=45;x<=90;x++){
-            setClock=(minAngle+((maxAngle-minAngle)/90*x));
-            clrClock=frequency-setClock;
-            *(motor180z+7)=(0x01<<GPIO);
-            udelay(setClock);
-            *(motor180z+10)=(0x01<<GPIO);
-            udelay(clrClock);
-        }
-    }
+	if(tmp_buf==2){
+		for(x=90;x>=45;x--){
+			setClock=(minAngle+((maxAngle-minAngle)/90*x));
+			clrClock=frequency-setClock;
+			*(motor180z+7)=(0x01<<GPIO);
+			udelay(setClock);
+			*(motor180z+10)=(0x01<<GPIO);
+			udelay(clrClock);
+		}
+		while(time_before(jiffies, delay)){}
+		for(x=45;x<=90;x++){
+			setClock=(minAngle+((maxAngle-minAngle)/90*x));
+			clrClock=frequency-setClock;
+			*(motor180z+7)=(0x01<<GPIO);
+			udelay(setClock);
+			*(motor180z+10)=(0x01<<GPIO);
+			udelay(clrClock);
+		}
+	}
 
-    if(tmp_buf==3){
-        for(x=0;x<=30;x++){
-            setClock=(minAngle+((maxAngle-minAngle)/30*x));
-            clrClock=frequency-setClock;
-            *(motor180z+7)=(0x01<<GPIO);
-            udelay(setClock);
-            *(motor180z+10)=(0x01<<GPIO);
-            udelay(clrClock);
-        }
-        while(time_before(jiffies, delay)){}
-        for(x=30;x>=0;x--){
-            setClock=(minAngle+((maxAngle-minAngle)/30*x));
-            clrClock=frequency-setClock;
-            *(motor180z+7)=(0x01<<GPIO);
-            udelay(setClock);
-            *(motor180z+10)=(0x01<<GPIO);
-            udelay(clrClock);
-        }
-    }
+	if(tmp_buf==3){
+		for(x=0;x<=30;x++){
+			setClock=(minAngle+((maxAngle-minAngle)/30*x));
+			clrClock=frequency-setClock;
+			*(motor180z+7)=(0x01<<GPIO);
+			udelay(setClock);
+			*(motor180z+10)=(0x01<<GPIO);
+			udelay(clrClock);
+		}
+		while(time_before(jiffies, delay)){}
+		for(x=30;x>=0;x--){
+			setClock=(minAngle+((maxAngle-minAngle)/30*x));
+			clrClock=frequency-setClock;
+			*(motor180z+7)=(0x01<<GPIO);
+			udelay(setClock);
+			*(motor180z+10)=(0x01<<GPIO);
+			udelay(clrClock);
+		}
+	}
 
-    if(tmp_buf==4){
-        printk("Activate 4");
-    }
+	if(tmp_buf==4){
+		printk("Activate 4");
+	}
 
-    if(tmp_buf==5){
-        printk("Activate 5");
-    }
+	if(tmp_buf==5){
+		printk("Activate 5");
+	}
 
-    if(tmp_buf==6){
-        printk("Activate 6");
-    }
-    return length;
+	if(tmp_buf==6){
+		printk("Activate 6");
+	}
+	return length;
 }
 
 
 static int motor180z_release(struct inode *minode, struct file *mfile){
-    motor180z_usage=0;
-    if(motor180z) iounmap(motor180z);
-    return 0;
+	motor180z_usage=0;
+	if(motor180z) iounmap(motor180z);
+	return 0;
 }
