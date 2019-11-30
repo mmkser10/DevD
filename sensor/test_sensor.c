f137c23a5305ee1244802dee8711f821ecb5176d
mmkser10 <mmkser10@gmail.com>
2019-11-30 19:14:33 +0900

    A

--- sensor/test_sensor.c
+++ sensor/test_sensor.c
@@ -93,23 +93,27 @@ int main()
     r_value=(r_value/10);
     b_value=(b_value/10);
     g_value=(g_value/10);
+    
+    printf("Red..... %f     ", r_value);
+    printf("Blue..... %f     ", b_value);
+    printf("Green..... %f     \n", g_value);
 
     if(11000 < r_value && 14000 > r_value && 7000 < b_value && 9000 > b_value && 6000 < g_value && 8000 > g_value){
         printf("Red");
     }
-    if(10000 < r_value && 12000 > r_value && 10000 < b_value && 14000 > b_value && 7000 < g_value && 9000 > g_value){
+    else if(10000 < r_value && 12000 > r_value && 10000 < b_value && 14000 > b_value && 7000 < g_value && 9000 > g_value){
         printf("Purple");
     }
-    if(5000 < r_value && 7000 > r_value && 9000 < b_value && 13000 > b_value && 5500 < g_value && 8500 > g_value){
+    else if(5000 < r_value && 7000 > r_value && 9000 < b_value && 13000 > b_value && 5500 < g_value && 8500 > g_value){
         printf("Blue");
     }
-    if(30000 < r_value && 35000 > r_value && 16000 < b_value && 20000 > b_value && 19000 < g_value && 23000 > g_value){
+    else if(30000 < r_value && 35000 > r_value && 16000 < b_value && 20000 > b_value && 19000 < g_value && 23000 > g_value){
         printf("Yellow");
     }
-    if(18000 < r_value && 22000 > r_value && 9500 < b_value && 11500 > b_value && 9000 < g_value && 11000 > g_value){
+    else if(18000 < r_value && 22000 > r_value && 9500 < b_value && 11500 > b_value && 9000 < g_value && 11000 > g_value){
         printf("Purple");
     }
-    if(8000 < r_value && 10000 > r_value && 11000 < b_value && 14000 > b_value && 9000 < g_value && 12000 > g_value){
+    else if(8000 < r_value && 10000 > r_value && 11000 < b_value && 14000 > b_value && 9000 < g_value && 12000 > g_value){
         printf("Purple");
     }
 
