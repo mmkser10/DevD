3651e861647e97605b90f82d14291c80b8feeefe
mmkser10 <mmkser10@gmail.com>
2019-11-30 19:31:57 +0900

    A

---
+++ RGBDriver_jdbackup/.sensor_driver.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/raspberry/DevD/RGBDriver_jdbackup/sensor_driver.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /root/raspberry/DevD/RGBDriver_jdbackup/sensor_driver.ko /root/raspberry/DevD/RGBDriver_jdbackup/sensor_driver.o /root/raspberry/DevD/RGBDriver_jdbackup/sensor_driver.mod.o
---
+++ RGBDriver_jdbackup/.tmp_versions/sensor_driver.mod
@@ -0,0 +1,2 @@
+/root/raspberry/DevD/RGBDriver_jdbackup/sensor_driver.ko
+/root/raspberry/DevD/RGBDriver_jdbackup/sensor_driver.o
